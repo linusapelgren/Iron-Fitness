04c140de5d70e10637b55d9253ebf8950a27a472
Linus Apelgren <linusapelgren6789@gmail.com>
2024-07-24 09:45:14 +0000

    Removed shop app and updated contact app

--- shop/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Product  # Adjust import as per your project structure
-
-def supplements(request):
-    products = Product.objects.all()  # Ensure Product is assigned here
-    return render(request, 'shop/supplements.html', {'products': products})
