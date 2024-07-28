dc5a35d2ede8cd43ef42a29b4708abf41744d8ed
Linus Apelgren <linusapelgren6789@gmail.com>
2024-07-28 22:32:04 +0000

    Update static files configuration

--- iron_fitness/settings.py
+++ iron_fitness/settings.py
@@ -173,6 +173,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]   
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
