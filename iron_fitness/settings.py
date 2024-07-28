8e4263cf6a6a0dc15265a3e0ef474c14e4c42039
Linus Apelgren <linusapelgren6789@gmail.com>
2024-07-28 22:19:15 +0000

    Installed whitenoise

--- iron_fitness/settings.py
+++ iron_fitness/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'iron_fitness.urls'
