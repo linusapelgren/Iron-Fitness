a854009679cb5ce0751d8c05730c4ac87cfedd81
Linus Apelgren <linusapelgren6789@gmail.com>
2024-11-24 18:53:21 +0000

    Updated profile page profile picture container

--- users/models.py
+++ users/models.py
@@ -9,9 +9,6 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, blank=True)
     gym_visits = models.IntegerField(default=0)
-    picture = models.ImageField(
-        upload_to="profile_pics/", default="profile_pics/blank_profile.png"
-    )
     street_address = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
     postal_code = models.CharField(max_length=20, blank=True, null=True)
