4e37d71909bcb726dab9d2ff21eb935bbc7b18ae
Linus Apelgren <linusapelgren6789@gmail.com>
2025-01-20 12:17:32 +0000

    Made migrations and made minor changes to navigation

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
+from users.models import UserProfile
 
 class NewsletterSignupForm(forms.Form):
-    email = forms.EmailField(label='Email Address', required=True, widget=forms.EmailInput(attrs={'placeholder': 'Enter your email'}))
+    subscribe = forms.BooleanField(
+        label="Subscribe to Newsletter",
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'subscribe-checkbox'})
+    )
+
+    def save(self, user):
+        # Get or create the user's profile
+        profile, created = UserProfile.objects.get_or_create(user=user)
+        profile.is_subscribed_to_newsletter = self.cleaned_data['subscribe']
+        profile.save()
